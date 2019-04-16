24b4a3f263e21a0639b3170c6c3ebe6e8c359b58
mlrv <marco.levrero@gmail.com>
2019-04-16 17:09:30 +0100

    Error handling chapter

---
+++ src/main/scala/errorhandling/Option.scala
@@ -0,0 +1,32 @@
+package option
+
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(a) => Some(f(a))
+    case _ => None
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case Some(a) => f(a)
+    case _ => None
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(a) => a
+    case _ => default
+  }    
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
+    case _ => this
+}
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(a) if f(a) => this
+    case _ => None
+  }
+
+}
+
+case class Some [+A] (get: A) extends Option[A]
+case object None extends Option[Nothing]
