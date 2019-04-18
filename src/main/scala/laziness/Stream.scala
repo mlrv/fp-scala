020f7aa2f4ca488b69dc808c7ddeb95656f25c81
mlrv <marco.levrero@gmail.com>
2019-04-18 16:18:29 +0100

    Stream DT

---
+++ src/main/scala/laziness/Stream.scala
@@ -0,0 +1,16 @@
+package laziness
+
+sealed trait Stream[+A]
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  } 
+  def empty[A]: Stream[A] = empty
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+}
