237f35b81d32528b434454ecbb0d96cb1c1e5008
mlrv <marco.levrero@gmail.com>
2019-04-21 17:51:49 +0100

    Stream functions

--- src/main/scala/laziness/Stream.scala
+++ src/main/scala/laziness/Stream.scala
@@ -1,10 +1,43 @@
 package laziness
+import Stream._
 
 sealed trait Stream[+A] {
   def toList: List[A] = this match {
     case Cons(h, t) => h() :: t().toList 
     case Empty => List()
   }
+
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case _ => empty
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 1 => t().drop(n - 1)
+    case _ => this
+  }
+
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h()) => cons(h(), t().takeWhile(p))
+    case _ => empty
+  }
+
+  def exist(p: A => Boolean): Boolean = this match {
+    case Cons(h, t) => p(h()) || t().exist(p)
+    case _ => false
+  }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+    case Cons(h, t) => f(h(), t().foldRight(z)(f)) 
+    case _ => z
+  }
+
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true)((a, b)=> p(a) && b)
+
+  def takeWhileViaFoldRight(p: A => Boolean): Stream[A] =
+    foldRight(empty[A])((h, t) => if (p(h)) cons(h, t) else empty)
 }
 
 case object Empty extends Stream[Nothing]
