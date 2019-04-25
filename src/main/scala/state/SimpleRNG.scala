a170294e01e0f5977b29008cf7276bb39d6a8bc0
mlrv <marco.levrero@gmail.com>
2019-04-25 11:13:33 +0100

    Till 6.5

--- src/main/scala/state/SimpleRNG.scala
+++ src/main/scala/state/SimpleRNG.scala
@@ -20,6 +20,57 @@ object RNG {
     case x => x
   }
 
+  def double(rng: RNG): (Double, RNG) = {
+    val (n, r) = nonNegativeInt(rng)
+    (n / (Int.MaxValue).toDouble, r)
+  }
+
+  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+    val (i, r1) = rng.nextInt
+    val (d, r2) = double(r1)
+    ((i, d), r2)
+  }
+
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    val (i, r1) = rng.nextInt
+    val (d, r2) = double(r1)
+    ((d, i), r2)
+  }
+
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1,d2,d3), r3)
+  }
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count == 0) (List(), rng)
+    else {
+      val (i1, r1) = rng.nextInt
+      val (is, r2) = ints(count - 1)(r1)
+      (i1 :: is, r2)
+    }
+  }
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def nonNegativeEven: Rand[Int] =
+    map(nonNegativeInt)(i => i - i % 2)
+
+  def doubleViaRand: Rand[Double] =
+    map(nonNegativeInt)(i => i / Int.MaxValue.toDouble)
+
 }  
 
 
