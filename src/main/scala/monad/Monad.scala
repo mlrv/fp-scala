692ed966999838ddcc47802be7020e7b88c8fa34
mlrv <marco.levrero@gmail.com>
2019-04-30 22:04:30 +0100

    Monoids & Monads

---
+++ src/main/scala/monad/Monad.scala
@@ -0,0 +1,51 @@
+package monad
+
+import laziness._
+
+trait Functor[F[_]] {
+  def map[A,B](fa: F[A])(f: A => B): F[B]
+}
+
+trait Monad[F[_]] extends Functor[F] {
+  def unit[A](a: => A): F[A]
+  def flatMap[A,B](ma: F[A])(f: A => F[B]): F[B]
+
+  def map[A,B](ma: F[A])(f: A => B): F[B] =
+    flatMap(ma)(a => unit(f(a)))
+
+  def map2[A,B,C](ma: F[A], mb: F[B])(f: (A,B) => C): F[C] =
+    flatMap(ma)(a => map(mb)(b => f(a, b)))
+
+  def sequence[A](lma: List[F[A]]): F[List[A]] =
+    lma.foldRight(unit(List[A]()))((ma, mla) => map2(ma, mla)(_ :: _))
+
+  def traverse[A,B](la: List[A])(f: A => F[B]): F[List[B]] =
+    la.foldRight(unit(List[B]()))((a, mlb) => map2(f(a), mlb)(_ :: _))
+
+  def replicateM[A](n: Int, ma: F[A]): F[List[A]] =
+    sequence(List.fill(n)(ma))
+
+  def compose[A,B,C](f: A => F[B], g: B => F[C]): A => F[C] =
+    (a: A) => flatMap(f(a))(g)
+}
+
+object Monad {
+  val optionMonad = new Monad[Option] {
+    def unit[A](a: => A) = Some(a)
+    override def flatMap[A,B](ma: Option[A])(f: A => Option[B]): Option[B] =
+      ma.flatMap(f)
+  }
+
+  val streamMonad = new Monad[Stream] {
+    def unit[A](a: => A): Stream[A] = Stream.cons(a, Empty)
+    override def flatMap[A,B](ma: Stream[A])(f: A => Stream[B]): Stream[B] =
+      ma.flatMap(f)
+  }
+
+
+  val listMonad = new Monad[List] {
+    def unit[A](a: => A): List[A] = List(a)
+    override def flatMap[A,B](ma: List[A])(f: A => List[B]): List[B] =
+      ma.flatMap(f)
+  }
+}
---
+++ src/main/scala/monoid/Monoid.scala
@@ -0,0 +1,67 @@
+package monoid
+
+trait Monoid[A] {
+  def op(a1: A, a2: A): A
+  def zero: A
+}
+
+object Monoid {
+
+  val stringMonoid = new Monoid[String] {
+    def op(a1: String, a2: String) = a1 + a2
+    val zero = ""
+  }
+
+  def listMonoid[A] = new Monoid[List[A]] {
+    def op(a1: List[A], a2: List[A]) = a1 ++ a2
+    val zero = Nil
+  }
+
+  val intAddition = new Monoid[Int] {
+    def op(a1: Int, a2: Int) = a1 + a2
+    val zero = 0
+  }
+
+
+  val intMultiplication = new Monoid[Int] {
+    def op(a1: Int, a2: Int) = a1 * a2
+    val zero = 1
+  }
+
+  val booleanOr = new Monoid[Boolean] {
+    def op(a1: Boolean, a2: Boolean) = a1 || a2
+    val zero = false
+  }
+
+
+  val booleanAnd = new Monoid[Boolean] {
+    def op(a1: Boolean, a2: Boolean) = a1 && a2
+    val zero = true
+  }
+
+  def optionMonoid[A] = new Monoid[Option[A]] {
+    def op(a1: Option[A], a2: Option[A]) = a1.orElse(a2)
+    val zero = None
+  }
+
+  def endoMonoid[A] = new Monoid[A => A] {
+    def op(a1: A => A, a2: A => A) = a1.compose(a2)
+    def zero = a => a
+  }
+
+  def concatenate[A](as: List[A], m: Monoid[A]): A =
+    as.foldLeft(m.zero)(m.op)
+
+  def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B =
+    as.map(f).foldLeft(m.zero)(m.op)
+
+  def foldMapV[A, B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): B =
+    if (v.length == 0) m.zero
+    else if (v.length == 1) f(v(0))
+    else {
+      val (l, r) = v.splitAt(v.length / 2)
+      m.op(foldMapV(l, m)(f), foldMapV(r, m)(f))
+    }
+    
+}
+
